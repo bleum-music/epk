ac08ed2a66a7501323ac062b9595a0ba98221761
jordanmceachern <jormceachern@gmail.com>
2019-02-18 17:43:41 -0400

    finished animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '../components/layout.css'
-import Social from '../components/social/social'
-import Song from '../components/song/song'
+import facebook from '../images/facebook.png'
+import instagram from '../images/instagram.png'
 // import portrait from '../images/banner.jpg'
 // import light_portrait from '../images/banner_light.jpg'
 import daniel from '../images/good_2.jpg'
@@ -11,10 +11,11 @@ import light_brittany from '../images/light_3.jpg'
 import logo_blue from '../images/logo_blue.png'
 import logo_pink from '../images/logo_pink.png'
 import logo_white from '../images/logo_white.png'
+import Arrow from '../components/arrow/arrow.js'
+import Song from '../components/song/song'
 import ignoredYouGraphic from '../images/ignoredyou.png'
 import isItLoveGraphic from '../images/isitlove.png'
 import moveWithMeGraphic from '../images/movewithme.png'
-import Arrow from '../components/arrow/arrow.js'
 // import ignoredYou from './ignoredyou.mp3'
 // import isItLove from './isitlove.mp3'
 // import moveWithMe from './movewithme.mp3'
@@ -22,7 +23,6 @@ import test from '../newBeginningInst.m4a'
 const ignoredYou = test
 const isItLove = test
 const moveWithMe = test
-
 
 class App extends React.Component {
   state = {
@@ -47,6 +47,40 @@ class App extends React.Component {
     third.onload = () => {this.setState({loadThird: true})}
   }
 
+  nullHandler = (id, classN) => {
+    if(document.getElementById(id) !== null) {
+      this.animate(id, classN).init()
+    }
+  }
+  animate = (id, classN) => {
+    var target
+    var windowHeight
+    const init = () => {
+      target = document.getElementById(id)
+      windowHeight = window.innerHeight
+      addEventHandlers()
+      checkPosition()
+    }
+    const addEventHandlers = () => {
+      window.addEventListener('scroll', checkPosition)
+      window.addEventListener('resize', init)
+    }
+    const checkPosition = () => {
+      var distanceFromTop = target.getBoundingClientRect().top
+      var distanceFromBottom = target.getBoundingClientRect().bottom
+      if (windowHeight - distanceFromTop <= 0 || windowHeight - distanceFromBottom > windowHeight) {
+        target.classList.add('hidden')
+        target.classList.remove(classN)
+      } else {
+        target.classList.add(classN)
+        target.classList.remove('hidden')
+      }
+    }
+    return {
+      init: init
+    }
+  }
+
   render () {
     var animateLogo = () => {
       var target
@@ -56,6 +90,7 @@ class App extends React.Component {
       var logoWhite
       var logoBlue
       var bio
+      var windowHeight
       const init = () => {
         target = document.getElementById('logo')
         banner = document.getElementById('banner')
@@ -63,7 +98,7 @@ class App extends React.Component {
         logoWhite = document.getElementById('logoWhite')
         logoBlue = document.getElementById('logoBlue')
         svg = document.getElementById('svg')
-        bio = Array.from(document.querySelectorAll('.bio'))
+        windowHeight = window.innerHeight
         addEventHandlers()
         checkPosition()
       }
@@ -73,49 +108,40 @@ class App extends React.Component {
       }
       const checkPosition = () => {
         var distanceFromTop = banner.getBoundingClientRect().top
-        if (distanceFromTop < 0) {
-          target.className = target.className.replace(
-            'hidden',
-            'logo'
-          )
+        var distanceFromBottom = banner.getBoundingClientRect().bottom
+        if (windowHeight - distanceFromTop > windowHeight && windowHeight + distanceFromBottom > windowHeight) {
+          target.classList.add('logo')
+          target.classList.remove('hidden')
           logoPink.classList.add('logoPink')
           logoWhite.classList.add('logoWhite')
           logoBlue.classList.add('logoBlue')
-          bio.forEach(p => {
-            p.classList.add('bioAnimate')
-            p.classList.remove('hidden')
-          })
           svg.style.opacity = '0'
           svg.style.height = '0'
         } 
+        else if (distanceFromTop >= 0) {
+          svg.style.opacity = '1'
+          svg.style.height = '100%'
+        }
         else {
-          target.className = target.className.replace(
-            'logo',
-            'hidden'
-          )
+          target.classList.add('hidden')
+          target.classList.remove('logo')
           logoPink.classList.remove('logoPink')
           logoWhite.classList.remove('logoWhite')
           logoBlue.classList.remove('logoBlue')
-          bio.forEach(p => {
-            p.classList.add('hidden')
-            p.classList.remove('bioAnimate')
-          })
-          svg.style.opacity = '1'
-          svg.style.height = '100%'
         }
       }
       return {
         init: init
       }
     }
-    const mounted = document.getElementById('banner')
-    if(mounted !== null) {
+    const banner = document.getElementById('banner')
+    if(banner !== null) {
       animateLogo().init()
     }
     
     // let logoPlaceHolder = <div className='logo'>loading...</div>
     // if(this.state.logoLoaded){logoPlaceHolder = <div className='logo'>
-    const logo = <div id='logo' className='hidden'>
+    const logo = <div id='logo'>
       <img id='logoPink' src={logo_pink} alt='brand logo'/>
       <img id='logoWhite' src={logo_white} alt='brand logo'/>
       <img id='logoBlue' src={logo_blue} alt='brand logo'/>
@@ -136,43 +162,73 @@ class App extends React.Component {
           <div id='svg'>
             <Arrow />
           </div>
-          <p className='bio'>Bleum is an electronic music duo consisting of producer Daniel James
-          and singer/ songwriter Brittany McQuinn. They began working together in
-          2016 after Daniel saw Brittany perform live. Impressed by her pop sensibility,
-          he urged her to send him some vocal tracks.</p>
-          <p className='bio'>They haven't stopped making music since. After the release of Brittanyʼs
-          solo EP, Bold (produced mostly by Daniel), they tried something untethered
-          from each others individual identities. The first single 'Lose You' was released
-          and Bleum was born. They've gone on to perform at multiple festivals. Their
-          latest single 'Visit' was selected to be on Blundstone Canada's online playlist.</p>
+          <div id='bio' onLoad={this.nullHandler('bio','bio')}>
+            <p className='bio'>Bleum is an electronic music duo consisting of producer Daniel James
+            and singer/ songwriter Brittany McQuinn. They began working together in
+            2016 after Daniel saw Brittany perform live. Impressed by her pop sensibility,
+            he urged her to send him some vocal tracks.</p>
+            <p className='bio'>They haven't stopped making music since. After the release of Brittanyʼs
+            solo EP, Bold (produced mostly by Daniel), they tried something untethered
+            from each others individual identities. The first single 'Lose You' was released
+            and Bleum was born. They've gone on to perform at multiple festivals. Their
+            latest single 'Visit' was selected to be on Blundstone Canada's online playlist.</p>
+          </div>
         </div>
         <div id='music'>
           <div className='spacer' />
           <div className='title'>music</div>
-          <div className='singles'>
+          <div id='songs' className='singles' onLoad={this.nullHandler('songs','songs')}>
             <Song graphic={ignoredYouGraphic} song={ignoredYou} title='ignored you' id='ignore' index='0' />
             <Song graphic={isItLoveGraphic} song={isItLove} title='is it love' id='isIt' index='1' />
             <Song graphic={moveWithMeGraphic} song={moveWithMe} title='move with me' id='move' index='2' />
           </div>
           <iframe
+              id='iframe'
               title='bleum live'
               src='https://www.youtube.com/embed/Vrg7yaniKn4'
               frameBorder='0'
               allow='accelerometer; encrypted-media; gyroscope; picture-in-picture'
-              allowFullScreen>
+              allowFullScreen
+              onLoad={this.nullHandler('iframe','video')}>
           </iframe>
         </div>
         <div id='social'>
           <div className='spacer' />
           <div>social media</div>
-          <img src={social} alt='daniel' />
-          <Social />
+          <img id='dan' src={social} alt='daniel' onLoad={this.nullHandler('dan','image')}/>
+          <div id='socialIcons'>
+            <div id='facebook' className='links' onLoad={this.nullHandler('facebook','faceAnimate')}>
+              <a
+                href='https://www.facebook.com/bleummusic/?ref=br_rs'
+                target='_blank'
+                rel='noopener noreferrer'>
+                <img id='fb' src={facebook} alt='link to facebook' />
+              </a>
+            </div>
+            <div id='instagram' className='links' onLoad={this.nullHandler('instagram','instaAnimate')}>
+              <a
+                href='https://www.instagram.com/bleummusic/?hl=en'
+                target='_blank'
+                rel='noopener noreferrer'>
+                <img src={instagram} alt='link to instagram' />
+              </a>
+            </div>
+            <div id='buyDiv' className='links' onLoad={this.nullHandler('buyDiv','buyAnimate')}>
+              <a
+                href='https://fanlink.to/visitbybleum'
+                target='_blank' rel='noopener noreferrer'
+                id='buy'>
+                <p>stream/ </p>
+                <p>buy</p>
+              </a>
+            </div>
+          </div>
         </div>
         <div id='contact'>
           <div className='spacer' />
           <div>contact</div>
-          <img src={contact} alt='brittany' />
-          <p><a href='mailto:bleum.music@gmail.com' target='_top'>bleum.music@gmail.com</a></p>
+          <img id='britt' src={contact} alt='brittany' onLoad={this.nullHandler('britt','imageVV')}/>
+          <p><a id='mail' href='mailto:bleum.music@gmail.com' target='_top' onLoad={this.nullHandler('mail','mail')}>bleum.music@gmail.com</a></p>
         </div>
       </div>
     )
