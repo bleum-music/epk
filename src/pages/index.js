2711b547adfa69d67ec15aa07565ebb79e540291
jordanmceachern <jormceachern@gmail.com>
2019-02-18 18:16:27 -0400

    trying to deal with gatsby document problem

--- src/pages/index.js
+++ src/pages/index.js
@@ -146,7 +146,7 @@ class App extends React.Component {
         init: init
       }
     }
-    if (document) {
+    if (document !== undefined) {
       const banner = document.getElementById('banner')
       if(banner !== null) {
         animateLogo().init()
