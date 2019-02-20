71391812aa56ea6ff223bcd08e11adf0fbf16cf8
jordanmceachern <jormceachern@gmail.com>
2019-02-19 20:10:33 -0400

    changed audio player style

--- src/components/song/song.js
+++ src/components/song/song.js
@@ -11,11 +11,12 @@ class Song extends React.Component {
   playHandler = progressClass => () => {
     var aud = document.getElementById(this.props.id)
     // var playB = document.getElementsByClassName('play-pause')[Number(this.props.index)]
-    if (progressClass === 'black') {
-      var progress = document.getElementsByClassName(progressClass)[Number(this.props.index)]
-    } else {
-      progress = document.getElementsByClassName(progressClass)[0]
-    }
+    // if (progressClass === 'black') {
+    var progress = document.getElementsByClassName('black')[Number(this.props.index)]
+    // ^about line^ 'black' used to = 'progressClass'
+    // } else {
+    //   progress = document.getElementsByClassName(progressClass)[0]
+    // }
 
     if (aud.paused) {
       aud.play()
@@ -31,11 +32,11 @@ class Song extends React.Component {
   }
 
   render () {
-    const progressClass = (this.props.index < 2)? 'black':'white'
+    // const progressClass = (this.props.index < 2)? 'black':'white'
     return (
       <React.Fragment>
         <audio id={this.props.id} src={this.props.song}>browser doesn't support audio</audio>
-        <div className='songContainer' onClick={this.playHandler(progressClass)} >
+        <div className='songContainer' onClick={this.playHandler()}> {/* (progressClass)} > */}
           <img src={this.props.graphic} alt={this.props.title}/>
           <div className='player'>
             {/* <div className='info'>
@@ -44,8 +45,9 @@ class Song extends React.Component {
             <div className='btns'>
               <img className='play-pause' src={this.state.src} alt='play or pause'/>
             </div>
+            <div className='black' /> {/*className used to = 'progressClass' */}
           </div>
-          <div className={progressClass} />
+          {/* <div className='black' /> className used to = 'progressClass' */}
         </div>
       </React.Fragment>
     )
